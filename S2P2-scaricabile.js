1fdcdbe21ea0125744dd9bf97d00aa6b98474320
Roberto Allocco <r.allocco@pragmos.it>
2024-03-22 20:23:19 +0100

    added solutions to homework

--- S2P2-scaricabile.js
+++ S2P2-scaricabile.js
@@ -16,6 +16,16 @@ ES. L'utente Marco Rossi e' un ambassador, quindi la frase dovrebbe essere "Marc
 Infine, crea un SECONDO array in cui inserirai SOLO gli ambassador.
 */
 
+/* PERSONAL NOTES:
+    - isAmbassador === true, apply 30% discount BEFORE applying shipping costs
+    - isAmbassador === false, no discount
+    - if shoppingCart > 100, apply free shipping, otherwise add shpping costs
+
+    warning: i'm not testing for input types because i assume that i will be the only one to input arguments into these functions,
+    and i know what they expect to get as input.
+    In a real project, it would be proper to test for input types before entering the calculations inside the functions.
+*/
+
 const marco = {
   name: "Marco",
   lastName: "Rossi",
@@ -37,3 +47,86 @@ const amy = {
 const prices = [34, 5, 2]
 const shippingCost = 50
 let utenteCheEffettuaLAcquisto = amy //cambia il valore qui per provare se il tuo algoritmo funziona!
+
+// i could be defining the array here like this: let users = [marco, paul, amy], but the specifics of the homework 
+// said to use push, so i'll do that
+let users = []
+
+users.push(marco, paul, amy)
+
+// this function has an object user as parameter. If the isAmbassador property is true, than it prints "it's an ambassodr",
+// otherwise "it's not an ambassador"
+function userIsAmbassador(user){
+  return user.isAmbassador ? 
+    console.log(`${user.name} ${user.lastName} is an ambassador`) : 
+    console.log(`${user.name} ${user.lastName} is not an ambassador`)
+}
+
+// printing for every user if it's an ambassador or not
+console.log('Printing users array:\n')
+for(let user of users){
+  userIsAmbassador(user)
+}
+
+// array for storing only ambassador users
+let ambassadorUsers =[]
+
+// inserting into ambassadorUsers only the users that are ambassador
+for(let user of users){
+  user.isAmbassador ? 
+    ambassadorUsers.push(user) : 
+    null
+}
+
+// function to sum up an array of prices
+function addPrices(prices){
+  let total = 0
+  for (let p of prices){
+    total += p
+  }
+  return total
+}
+
+// function to calculate final checkout total, applying discount if the user is an ambassador
+function checkout(user, prices){
+  // adding prices
+  let actualTotal = addPrices(prices)
+  // applying discount if ambassador
+  actualTotal = user.isAmbassador ? 
+  actualTotal *= 0.7: 
+  actualTotal
+  // if actuatlTotal > 100, return actualTotal, otherwise, reuse addPrices function to add to the total the shippingCost
+  return actualTotal > 100 ?
+    actualTotal:
+    addPrices([actualTotal,shippingCost])
+}
+
+
+
+// test cases
+let totalPrice = 0
+// base case
+console.log(`\nTesting base case Amy, [34, 5, 2]`)
+totalPrice = checkout(utenteCheEffettuaLAcquisto, prices).toFixed(2)
+console.log(`Your total is ${totalPrice}€`)
+
+// price over 100 and isAmbassador == true
+console.log(`\nTesting price over 100 and isAmbassador = true. User Marco, [150, 200, 10]`)
+prices2 = [150, 200, 10]
+utenteCheEffettuaLAcquisto2 = marco
+totalPrice = checkout(utenteCheEffettuaLAcquisto2, prices2).toFixed(2)
+console.log(`Your total is ${totalPrice}€`)
+
+// price under 100 and isAmbassador == true
+console.log(`\nTesting price under 100 and isAmbassador = true. User Marco, [10, 20, 5]`)
+prices3 = [10, 20, 5]
+utenteCheEffettuaLAcquisto3 = marco
+totalPrice = checkout(utenteCheEffettuaLAcquisto3, prices3).toFixed(2)
+console.log(`Your total is ${totalPrice}€`)
+
+// price over 100 and isAmbassador == false
+console.log(`\nTesting price under 100 and isAmbassador = false. User paul, [100, 20, 5]`)
+prices4 = [100, 20, 5]
+utenteCheEffettuaLAcquisto4 = paul
+totalPrice = checkout(utenteCheEffettuaLAcquisto4, prices4).toFixed(2)
+console.log(`Your total is ${totalPrice}€`)
